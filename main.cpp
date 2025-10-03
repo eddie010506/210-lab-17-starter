ca15bbb3db651ad3f584bc320d81c36800e51ca6
Edward Choi <edwardchoi2001@gmail.com>
2025-10-02 23:42:50 -0700

    lab17 final fix

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 const int SIZE = 7;  
@@ -76,7 +77,7 @@ void addNodeToFront(Node*& head, float val) {
     head = newNode;
 }
 
-void addNodeTotail(Node*& head, float val) {
+void addNodeToTail(Node*& head, float val) {
     Node *newNode = new Node;
     newNode->value = val;
     newNode->next = nullptr;
@@ -90,4 +91,67 @@ void addNodeTotail(Node*& head, float val) {
         current = current->next;
     }
     current->next = newNode;
+}
+
+void deleteNode(Node*& head, int position) {
+    if (!head || position < 1) { 
+        return;
+    }
+
+    Node* current = head;
+    if (position == 1) {
+        head = current->next; 
+        delete current;       
+        return;
+    }
+
+    
+    Node* prev = nullptr;
+    for (int i = 1; current != nullptr && i < position; i++) {
+        prev = current;
+        current = current->next;
+    }
+
+    if (current == nullptr) {
+        cout << "Position is out of bounds." << endl;
+        return;
+    }
+    
+   
+    prev->next = current->next;
+    delete current;
+}
+
+
+void insertNode(Node*& head, float val, int position) {
+    if (position < 1) { 
+        return;
+    }
+    
+    Node* prev = head;
+    for (int i = 1; i < position && prev != nullptr; i++) {
+        prev = prev->next;
+    }
+
+
+    if (prev != nullptr) {
+        Node *newNode = new Node;
+        newNode->value = val;
+        newNode->next = prev->next;
+        prev->next = newNode;
+    } else {
+        cout << "Position is out of bounds." << endl;
+    }
+}
+
+void deleteList(Node*& head) {
+    Node* current = head;
+    Node* nextNode = nullptr;
+    
+    while (current != nullptr) {
+        nextNode = current->next; 
+        delete current;           
+        current = nextNode;       
+    }
+    head = nullptr;
 }
